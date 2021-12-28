21a302334cc2b38ff3e8309d298f848dada910e7
Brayan Bermudez <bfbermudezc@gmail.com>
2021-12-28 10:55:21 -0500

    Inicio del proyecto desde cero

---
+++ app/src/main/java/com/platzi/android/firestore/ui/activity/LoginActivity.kt
@@ -0,0 +1,59 @@
+package com.platzi.android.firestore.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.platzi.android.firestore.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_trader.*
+
+/**
+ * @author Brayan Bermudez
+ * github brayancaselles
+ */
+
+
+const val USERNAME_KEY = "username_key"
+
+class LoginActivity : AppCompatActivity() {
+
+
+    private val TAG = "LoginActivity"
+    private var auth:FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+
+    fun onStartClicked(view: View) {
+        auth.signInAnonymously().addOnCompleteListener{
+            task ->
+            if(task.isSuccessful){
+                val userName = username.text.toString()
+                println("El nombre del Usuario es: $userName")
+                startMainActivity(userName)
+            }else{
+                showErrorMessage(view)
+            }
+        }
+        //startMainActivity("Santiago")
+    }
+
+    private fun showErrorMessage(view: View) {
+        Snackbar.make(view, getString(R.string.error_while_connecting_to_the_server), Snackbar.LENGTH_LONG)
+            .setAction("Info", null).show()
+    }
+
+    private fun startMainActivity(username: String) {
+        val intent = Intent(this@LoginActivity, TraderActivity::class.java)
+        intent.putExtra(USERNAME_KEY, username)
+        startActivity(intent)
+        finish()
+    }
+
+}
